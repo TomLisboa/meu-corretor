ea9908b5fb917532d9efe91d5b779913cbdc34a6
TomLisboa <159669054+TomLisboa@users.noreply.github.com>
2026-07-02 12:17:55 -0300

    Criar se.js

---
+++ se.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'corretor-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response; // Retorna do cache se estiver offline
+        return fetch(event.request);
+      })
+  );
+});
